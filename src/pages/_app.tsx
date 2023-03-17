ed2825c91784181a4c331d251f166d1d416bc410
Ryan King <ryan.king157@gmail.com>
2023-03-16 20:58:19 -0700

    all client rendering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+import type { AppType } from "next/app";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import dynamic from "next/dynamic";
 
 import { trpc } from "../utils/trpc";
 
@@ -10,9 +11,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  // Disable SSR for all pages
+  const DynamicComponent = dynamic(() => Promise.resolve(Component), {
+    ssr: false,
+  });
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <DynamicComponent {...pageProps} />
     </SessionProvider>
   );
 };
