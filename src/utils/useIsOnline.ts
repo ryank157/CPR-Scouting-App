197e1c8b506609a64d1ed809ff28b03a364f03aa
Ryan King <ryan.king157@gmail.com>
2023-03-16 21:21:18 -0700

    offline fix 5

--- src/utils/useIsOnline.ts
+++ src/utils/useIsOnline.ts
@@ -1,25 +1,40 @@
 import { useState, useEffect } from "react";
 
 const useIsOnline = () => {
-  const [isOnline, setIsOnline] = useState(true);
+  const [isOnlineStatus, setIsOnlineStatus] = useState(true);
 
   useEffect(() => {
     if (typeof window === "undefined") return;
 
-    const handleOnline = () => setIsOnline(true);
-    const handleOffline = () => setIsOnline(false);
+    const checkOnlineStatus = async () => {
+      const online = await isOnline();
+      setIsOnlineStatus(online);
+    };
 
-    setIsOnline(navigator.onLine);
-    window.addEventListener("online", handleOnline);
-    window.addEventListener("offline", handleOffline);
+    checkOnlineStatus();
+
+    // Check the online status every 10 seconds
+    const interval = setInterval(checkOnlineStatus, 10000);
 
     return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
+      clearInterval(interval);
     };
   }, []);
 
-  return isOnline;
+  return isOnlineStatus;
 };
 
 export default useIsOnline;
+
+async function isOnline() {
+  try {
+    const response = await fetch("https://www.google.com", {
+      mode: "no-cors",
+      cache: "no-cache",
+    });
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
